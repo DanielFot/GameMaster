4fbfa104e91546dfac21269df0c50fcff21fb4b8
DanielFot <danifot22@gmail.com>
2025-05-08 20:43:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
+None
+meme_dict = {
+            "OVERRATED": "Something that needs to be worse",
+            "UNDERRATED": "Something that needs to be better",
+            "CRINGE": "Something that is embarassing and not funny",
+            "LOL": "Something that is very funny",
+            "TYSM": "It means Thank You So Much",
+            "IDC": "It means I Dont Care",
+            "VIP": "Very Important Person"
+            }
 
+word = input("Write a non understandable word (With Large Characters!): ")
+
+if word in meme_dict_keys():
+   print(meme_dict[word])
+else:
+   print("Sorry, That word doesnt exist...")
