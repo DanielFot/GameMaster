3c9448f342d701fed168bb25104ad3c150cf7377
DanielFot <danifot22@gmail.com>
2025-05-19 14:16:05 +0200

    Create bot_logic.py

---
+++ bot_logic.py
@@ -0,0 +1,11 @@
+import random
+from bot_logic import gen_pass
+
+def gen_pass(pass_length):
+    elements = "+-/*!&$#?=@<>123456789"
+    password = ""
+    for i in range(pass_length):
+        password += random.choice(elements)
+    return password
+
+    print(gen_pass(17))
